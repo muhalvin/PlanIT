976a69999145cd4782066669e4e5a4ef288be7fb
Muhammad Alvin <muhammadalvin888@gmail.com>
2024-11-10 21:32:16 +0700

    Feat: add authentication

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return "API";
+Route::prefix('auth')->name('auth.')->group(function () {
+    Route::middleware('throttle:5,1')->group(function () {
+        Route::post('register', [App\Http\Controllers\AuthController::class, 'register'])->name('register');
+        Route::post('login', [App\Http\Controllers\AuthController::class, 'login'])->name('login');
+    });
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('logout', [App\Http\Controllers\AuthController::class, 'logout'])->name('logout');
+    });
 });
